00760516f5505a6d4b15aed7ad6ffaaf3c819de6
Oleg Gnat <banan20051@gmail.com>
2021-02-23 16:00:43 +0200

    first commit

---
+++ FibSequence2Proj/FibSequence2Proj/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace FibSequence2Proj
+{
+    class Program
+    {
+        static void Main()
+        {
+            foreach (var x in new FibSequence2(10))
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+
+    class FibSequence2 : IEnumerable
+    {
+        private int N;
+
+        public FibSequence2(int n)
+        {
+            N = n;
+        }
+        
+        public IEnumerator GetEnumerator()
+        {
+            int cur = 0;
+            int prev = 1;
+            for (int i = 0; i < N; i++)
+            {
+                yield return cur;
+                (prev, cur) = (cur, cur + prev);
+            }
+        }
+    }
+}
---
+++ FibSequenceProj/FibSequenceProj/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+
+namespace FibSequenceProj
+{
+    class Program
+    {
+        static void Main()
+        {
+            foreach (var x in new FibonacciSequence())
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+
+    class FibonacciIterator : IEnumerator
+    {
+        private int N;
+        private int i;
+        private int cur = 0;
+        private int prev = 1;
+        public FibonacciIterator(int n)
+        {
+            N = n;
+        }
+
+        public bool MoveNext()
+        {
+            if (i >= N)
+                return false;
+            (prev, cur) = (cur, prev + cur);
+            i++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Current
+        {
+            get{
+                return cur;
+            }
+        }
+    }
+
+    class FibonacciSequence : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            return new FibonacciIterator(10);
+        }
+    }
+}
